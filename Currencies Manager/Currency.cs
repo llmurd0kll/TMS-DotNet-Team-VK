beeb4130c78972ccfbebc7162b4655781592b0b8
KrisAnt <62154371+KrisAnt@users.noreply.github.com>
2020-04-23 15:17:01 +0300

    feat: created new Currencies Manager app
    1. Created classes and Interfaces

---
+++ Currencies Manager/Currency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Currencies_Manager
+{
+    class Currency
+    {
+        public string Abbreviation { get; set; }
+
+        public double Value { get; set; }
+
+        public int Scale { get; set; }
+    }
+}
